15255452f041070d7982b9bde2bb72574fcd0739
Hubertformin <Rosemary.com98>
2022-04-05 21:23:13 +0100

    Added sentry integration

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,45 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import morgan from 'morgan';
-import userRouter from './controllers/user.controller';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import userRouter from "./controllers/user.controller";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-//Connect to database
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => {
-  console.log("[SERVER] Connected to database!");
-})
-.catch(() => {
-  console.log("[SERVER] Unable to Connect to database!");
+Sentry.init({
+  dsn: process.env.SENTRY_DNS,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
 });
+
+//Connect to database
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("[SERVER] Connected to database!");
+  })
+  .catch(() => {
+    console.log("[SERVER] Unable to Connect to database!");
+  });
+
+// SENTRY RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 // Cors
 app.use(cors());
@@ -27,11 +51,13 @@ app.use(bodyParser.json());
 app.use(morgan("dev"));
 
 /**
- * 
+ *
  * ==== Routes ===
  */
-app.use('/users', userRouter);
+app.use("/users", userRouter);
 
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(port, () => {
   console.log(`[SERVER] Server is running on the port ${port}.`);
